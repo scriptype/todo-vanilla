6a115eb5deb581b7b7be6774c35eba4e596df88a
scriptype <scriptyper@gmail.com>
2020-01-04 04:14:32 +0200

    Create Footer component

---
+++ app/components/Footer.js
@@ -0,0 +1,11 @@
+import createView from '../lib/view.js';
+
+const template = () => `
+  <p>Created by Enes Ertarhanacı.</p>
+`;
+
+const FooterView = createView({
+  template
+});
+
+export default FooterView;
