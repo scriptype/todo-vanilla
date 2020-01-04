fc8ba9c4bcceb86e15ac10963ea60a2107a2deb8
scriptype <scriptyper@gmail.com>
2020-01-04 04:09:42 +0200

    Allow custom methods in lib/store

--- app/lib/store.js
+++ app/lib/store.js
@@ -1,17 +1,20 @@
 import createEventEmitter from './event-emitter.js';
 
-function createStore(initialData) {
+function createStore({ data, ...rest }) {
   const eventEmitter = createEventEmitter();
-
-  const data = {
-    ...initialData
-  };
 
   const get = key => data[key];
   const set = (key, value) => data[key] = value;
   const remove = key => delete data[key];
 
+  const ctx = { ...rest, get, set, remove };
+  const restWithContext = {};
+  Object.keys(rest).forEach(key => {
+    restWithContext[key] = rest[key].bind(ctx);
+  });
+
   return Object.freeze({
+    ...restWithContext,
     ...eventEmitter,
     get,
     set,
