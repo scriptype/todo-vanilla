79160007e9d8b7df9fe6427a9cd854688928d4e6
scriptype <scriptyper@gmail.com>
2020-01-04 04:14:07 +0200

    Allow custom methods in View
    - Provide context to renderFn (data, props, $)
    - Pass props to render as second argument

--- app/lib/view.js
+++ app/lib/view.js
@@ -1,16 +1,18 @@
-function createView({ template, data: dataFn, render: renderFn }) {
+function createView(options) {
+  const { template, data: dataFn, render: renderFn, ...rest } = options;
   return props => Object.freeze({
     render(parentNode) {
       const data = dataFn ? dataFn(props) : {};
       const html = template(data);
+      const ctx = { data, props, ...rest };
 
       if (parentNode.tagName === 'TEMPLATE') {
         const upperParent = parentNode.parentNode;
         parentNode.outerHTML = html;
-        renderFn && renderFn(upperParent, data);
+        renderFn && renderFn.call({ ...ctx, $: upperParent }, upperParent, data, props);
       } else {
         parentNode.innerHTML = html;
-        renderFn && renderFn(parentNode, data);
+        renderFn && renderFn.call({ ...ctx, $: parentNode }, parentNode, data, props);
       }
     }
   });
