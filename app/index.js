e83d33bc73d2aa12ce073c7edc834ffcbb971eb2
scriptype <scriptyper@gmail.com>
2020-01-04 19:21:43 +0200

    Add initial test todo and methods to store

--- app/index.js
+++ app/index.js
@@ -3,11 +3,28 @@ import MainView from './views/main/Main.js';
 
 const TodoStore = createStore({
   data: {
-    todos: [],
+    todos: [
+      { isCompleted: true, content: 'Write vanilla JS' }
+    ],
   },
   addTodo(todo) {
     this.set('todos', this.get('todos').concat(todo));
     return this;
+  },
+  toggleTodo(todo) {
+    this.updateTodo(todo, { isCompleted: !todo.isCompleted })
+  },
+  updateTodo(todo, values) {
+    const todos = this.get('todos');
+    const item = todos.find(t => t === todo);
+    const updatedTodo = {
+      ...item,
+      ...values
+    };
+    const index = todos.indexOf(item);
+    this.set('todos', Object.assign([], todos, {
+      [index]: updatedTodo
+    }));
   }
 });
 
