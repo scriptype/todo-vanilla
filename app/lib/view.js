310b15349cf058add28efc283c4242d8adc173a3
scriptype <scriptyper@gmail.com>
2020-01-04 02:57:45 +0200

    Simplify lib/view

--- app/lib/view.js
+++ app/lib/view.js
@@ -1,21 +1,17 @@
 function createView({ template, data: dataFn, render: renderFn }) {
-  const render = (parentNode, props) => {
-    const data = dataFn ? dataFn(props) : {};
-    const html = template(data);
-
-    if (parentNode.tagName === 'TEMPLATE') {
-      const upperParent = parentNode.parentNode;
-      parentNode.outerHTML = html;
-      renderFn && renderFn(upperParent, data);
-    } else {
-      parentNode.innerHTML = html;
-      renderFn && renderFn(parentNode, data);
-    }
-  };
-
   return props => Object.freeze({
     render(parentNode) {
-      return render(parentNode, props)
+      const data = dataFn ? dataFn(props) : {};
+      const html = template(data);
+
+      if (parentNode.tagName === 'TEMPLATE') {
+        const upperParent = parentNode.parentNode;
+        parentNode.outerHTML = html;
+        renderFn && renderFn(upperParent, data);
+      } else {
+        parentNode.innerHTML = html;
+        renderFn && renderFn(parentNode, data);
+      }
     }
   });
 };
