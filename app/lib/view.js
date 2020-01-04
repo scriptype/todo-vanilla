7e46d5e1f6c3c7b7718336d4f0655536beff90fc
scriptype <scriptyper@gmail.com>
2020-01-04 19:24:38 +0200

    Improve lib/view
    - Views can no longer access outside of their parentNode using $
    - Remove support for replacing parentNode in render
    - Provide a refresh method through ctx
    - Provide a way to render beforeEnd of parentNode

--- app/lib/view.js
+++ app/lib/view.js
@@ -1,18 +1,40 @@
+function getWrapperNode(html) {
+  const temp = document.createElement('div');
+  temp.innerHTML = html;
+  return temp.firstElementChild;
+};
+
 function createView({ template, data: dataFn, render: renderFn, ...rest }) {
   return props => Object.freeze({
-    render(parentNode) {
+    render(parentNode, mode) {
       const data = dataFn ? dataFn(props) : {};
       const html = template(data);
       const ctx = { data, props, ...rest };
 
-      if (parentNode.tagName === 'TEMPLATE') {
-        const upperParent = parentNode.parentNode;
-        parentNode.outerHTML = html;
-        renderFn && renderFn.call({ ...ctx, $: upperParent }, upperParent, data, props);
-      } else {
-        parentNode.innerHTML = html;
-        renderFn && renderFn.call({ ...ctx, $: parentNode }, parentNode, data, props);
+      if (!parentNode) {
+        return html;
       }
+
+      const wrapperNode = getWrapperNode(html);
+
+      if (!mode) {
+        parentNode.innerHTML = '';
+        parentNode.appendChild(wrapperNode);
+      } else if (mode === 'beforeEnd') {
+        parentNode.insertAdjacentElement('beforeEnd', wrapperNode);
+      }
+
+      const refresh = () => {
+        this.render(wrapperNode.parentNode, mode);
+      };
+
+      if (renderFn) {
+        renderFn.call({ ...ctx, $: wrapperNode, refresh }, wrapperNode, data, props);
+      }
+    },
+
+    toString() {
+      return this.render();
     }
   });
 };
