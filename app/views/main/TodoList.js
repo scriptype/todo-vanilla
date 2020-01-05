1ef54340889b280487b54e99554596099edd8349
scriptype <scriptyper@gmail.com>
2020-01-05 21:09:34 +0200

    Implement 'hide completed' feature

--- app/views/main/TodoList.js
+++ app/views/main/TodoList.js
@@ -2,8 +2,12 @@ import createView from '../../lib/view.js';
 import TodoItemView from './TodoItem.js';
 import AddTodoView from './AddTodoForm.js';
 
-const template = ({ todos }) => `
+const template = ({ todos, hideCompleted }) => `
   <div class="todo-list">
+    <label class="hide-completed">
+      <input type="checkbox" id="hide-completed" ${hideCompleted ? 'checked' : ''} />
+      hide completed
+    </label>
     <ul id="todos" class="todos"></ul>
     <div id="add-todo"></div>
   </div>
@@ -14,8 +18,18 @@ const TodoListView = createView({
 
   data({ TodoStore }) {
     return {
-      todos: TodoStore.get('todos')
+      todos: TodoStore.get('todos'),
+      hideCompleted: this.state.hideCompleted
     }
+  },
+
+  state: {
+    hideCompleted: false
+  },
+
+  onToggleHideCompleted(event) {
+    this.state.hideCompleted = !this.state.hideCompleted;
+    this.refresh();
   },
 
   onToggleTodo(todo) {
@@ -34,6 +48,9 @@ const TodoListView = createView({
   },
 
   renderTodo(todo) {
+    if (this.state.hideCompleted && todo.isCompleted) {
+      return;
+    }
     const todoItem = TodoItemView({
       todo,
       onToggleTodo: e => this.onToggleTodo(todo),
@@ -54,6 +71,9 @@ const TodoListView = createView({
     });
 
     addTodoForm.render($.querySelector('#add-todo'));
+
+    const $hideCompleted = $.querySelector('#hide-completed');
+    $hideCompleted.addEventListener('change', this.onToggleHideCompleted.bind(this));
   }
 });
 
